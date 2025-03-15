a0e784cf98a093456d41bb48448c4d360cff6365
Ivan Rechkin <rechkinin@gmail.com>
2025-03-15 17:40:09 +0500

    fallback Quake mode to Default in WezTerm

--- SSH/Terminal/WezTerm.cs
+++ SSH/Terminal/WezTerm.cs
@@ -7,9 +7,8 @@ public class WezTerm : ITerminalHandler
 	public static bool OpenTerminal(string host, string title, WindowMode mode) =>
 		mode switch
 		{
-			WindowMode.Default => Helper.OpenInShell("wezterm-gui", $"ssh {host}"),
+			WindowMode.Default or WindowMode.Quake => Helper.OpenInShell("wezterm-gui", $"ssh {host}"),
 			WindowMode.NewTab => Helper.OpenInShell("wezterm", $"cli spawn -- ssh {host}"),
-			WindowMode.Quake => false,
 			_ => throw new ArgumentOutOfRangeException(nameof(mode), "Impossible enum value"),
 		};
 }
